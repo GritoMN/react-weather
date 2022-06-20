b72452c8cbd0f7096ea40d8b50e87b9809ad5010
Gr1t0 <nikolova.grigoriya@gmail.com>
2022-06-20 11:26:06 +0200

    styling weather.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App">Weaher app</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <footer>
+        This project was coded by Grito and is {""}{" "}
+        <a href="https://github.com/GritoMN/portfolio-project" target="_blank">
+          open-sourced in Git Hub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
