c059ff967982d1a3a92945254ebed8940b9959a2
Gr1t0 <nikolova.grigoriya@gmail.com>
2022-06-27 21:02:40 +0200

    first day forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,7 @@
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherForecast from "./WeatherForecast";
+import WeatherIcon from "./WeatherIcon";
 import "./Weather.css";
 
 export default function WeatherInfo(props) {
@@ -16,7 +18,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <img src={props.data.iconUrl} alt={props.data.description} />{" "}
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
             <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
@@ -25,7 +27,6 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 15%</li>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
